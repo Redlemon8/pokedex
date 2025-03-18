6af6b121080ed0d47f5b100f990b0685f00b5623
redlemon8 <hanquezbenjamin@gmx.com>
2025-03-18 11:35:48 +0100

    test: modal

--- front/public/js/pokemon.js
+++ front/public/js/pokemon.js
@@ -1,32 +1,46 @@
 import api from './api.js';
 
 const pokemon = {
-    init() {
-        pokemon.load();
-    },
+  init() {
+    pokemon.load();
+    pokemon.handleModal()
+  },
 
-    async load() {
+  async load() {
 
-        const pokemons = await api.getPokemons();
-        console.log(pokemons);
+    const pokemons = await api.getPokemons();
+    console.log(pokemons);
 
-        if (pokemons === null) {
-            alert("Impossible de charger les pokemons !");
-            return;
-        }
+    if (pokemons === null) {
+        alert("Impossible de charger les pokemons !");
+        return;
+    }
 
-        pokemons.forEach(loadPokemon => pokemon.addToDOM(loadPokemon));
-    },
+    pokemons.forEach(loadPokemon => pokemon.addToDOM(loadPokemon));
+  },
 
-    addToDOM(pokemon) {
-        
-        const template = document.querySelector('.template-pokemon');
-        const clone = template.content.cloneNode(true);
-        clone.querySelector('[slot="pokemon-name"]').textContent = pokemon.name;
-        clone.querySelector(".pkm_img").src = "images/" + pokemon.id + ".webp";
+  addToDOM(pokemon) {
+      
+    const template = document.querySelector('.template-pokemon');
+    const clone = template.content.cloneNode(true);
+    clone.querySelector('[slot="pokemon-name"]').textContent = pokemon.name;
+    clone.querySelector(".pkm_img").src = "images/" + pokemon.id + ".webp";
+    clone.handleModal();
 
-        document.getElementById("app").append(clone);
-    },
+    document.getElementById("app").append(clone);
+  },
+
+  handleModal() {
+    const pkmDetail = document.getElementById("pkm_detail"); // modal
+    const pkmCard = document.querySelectorAll('.card'); // all pkm card
+
+    pkmCard.forEach((card) => {
+      card.addEventListener("click", () => {
+        pkmDetail.classList.add("is-active");
+      });
+    });
+
+  }
 };
 
 export default pokemon;
